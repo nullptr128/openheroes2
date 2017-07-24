7af60c511e971d354036734748bab6a338192be7
Martin Chyła <nullptr128@gmail.com>
2017-07-24 18:08:03 +0200

    fix build scripts on windows

--- scripts/editor-dev.js
+++ scripts/editor-dev.js
@@ -1,14 +1,17 @@
 
+//var childProcess = require( 'child_process' );
+//var devServer = childProcess.spawn( 'node' , [ 'node_modules/webpack-dev-server/bin/webpack-dev-server.js' , '--hot' , '--inline' , '--config' , './webpack/editor.dev.js' ] , { stdio: 'inherit' } );
+
 var childProcess = require( 'child_process' );
 
-var webpackLauncher = childProcess.spawn( 'webpack' , [ '--config' , './webpack/editor-launcher.js' ] , { stdio: 'inherit' } );
+var webpackLauncher = childProcess.spawn( 'node' , [ 'node_modules/webpack/bin/webpack.js' , '--config' , './webpack/editor-launcher.js' ] , { stdio: 'inherit' } );
 webpackLauncher.on( 'close' , () => {
 
-    var devServer = childProcess.spawn( 'webpack-dev-server' , [ '--hot' , '--inline' , '--config' , './webpack/editor.dev.js' ] , { stdio: 'inherit' } );
+    var devServer = childProcess.spawn( 'node' , [ 'node_modules/webpack-dev-server/bin/webpack-dev-server.js' , '--hot' , '--inline' , '--config' , './webpack/editor.dev.js' ] , { stdio: 'inherit' } );
     //devServer.stdout.on( 'data' , data => console.log( data.toString() ) );
 
     setTimeout( () => {
-        var electron = childProcess.spawn( 'electron' , [ './bin/editor/launch-editor.js' , '--dev' ] , { stdio: 'inherit' } );
+        var electron = childProcess.spawn( './node_modules/electron/dist/electron' , [ './bin/editor/launch-editor.js' , '--dev' ] , { stdio: 'inherit' } );
         //electron.stdout.on( 'data' , data => console.log( data.toString() ) );
     } , 3*1000 );
 
